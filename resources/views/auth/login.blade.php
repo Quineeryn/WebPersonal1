f0402e1da104cd5cef53db8f28850b9f0a3438ef
fathiaqurrata <fathiaqurrataaini@gmail.com>
2023-12-13 08:47:09 +0700

    verif login dan regis

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <!-- Coding By CodingNepal - codingnepalweb.com -->
 <html lang="en" dir="ltr">
@@ -5,29 +8,78 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Login | Cravitae </title>
+    <title>Login | Cravitae</title>
     <link rel="stylesheet" href="/cssfile/style.css">
+    <style>
+        .error-message {
+            color: red;
+            font-size: 10px;
+        }
+    </style>
 </head>
 
 <body>
     <div class="wrapper">
         <h2>Login</h2>
-        <form method="POST" action="#" enctype="multipart/form-data">
-          @csrf
+        <form method="POST" action="{{ route('login.store') }}" enctype="multipart/form-data" onsubmit="return validateForm()">
+            @csrf
             <div class="input-box">
                 <input type="text" name="email" placeholder="Enter your email" required>
+                <div id="email-error" class="error-message"></div>
             </div>
             <div class="input-box">
-                <input type="password" name="password" placeholder="Create password" required>
+                <input type="password" name="password" placeholder="Enter your password" required>
+                <div id="password-error" class="error-message"></div>
             </div>
             <div class="input-box button">
-                <input type="submit" value="Register Now">
+                <input type="submit" value="Login Now">
             </div>
             <div class="text">
-                <h3>Don't have account? <a href="/register">Login now</a></h3>
+                <h3>Don't have an account? <a href="/register">Register now</a></h3>
             </div>
         </form>
     </div>
+
+    <script>
+        function validateForm() {
+            var email = document.getElementsByName("email")[0].value;
+            var password = document.getElementsByName("password")[0].value;
+
+            // Clear previous error messages
+            document.getElementById('email-error').innerHTML = '';
+            document.getElementById('password-error').innerHTML = '';
+
+            // Check if the email is not empty and has a valid format
+            if (!email || !isValidEmail(email)) {
+                showAlert('Email yang anda masukan salah.', 'email-error');
+                return false;
+            }
+
+            // Check if the password is at least 8 characters
+            if (password.length < 8) {
+                showAlert('Password yang anda masukan salah.', 'password-error');
+                return false;
+            }
+
+            // You can add more checks here based on your requirements
+
+            // If all checks pass, return true to submit the form
+            return true;
+        }
+
+        function showAlert(message, errorDivId) {
+            // You can customize the alert appearance using SweetAlert or other libraries
+            document.getElementById(errorDivId).innerHTML = message;
+        }
+
+        function isValidEmail(email) {
+            // A simple email validation regex
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
+    </script>
+
 </body>
 
 </html>
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,26 +5,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Registration | Cravitae </title>
+    <title>Registration | Cravitae</title>
     <link rel="stylesheet" href="/cssfile/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <style>
+        .error-message {
+            color: red;
+            font-size: 10px;
+            margin-top: 5px;
+        }
+    </style>
 </head>
 
 <body>
     <div class="wrapper">
         <h2>Registration</h2>
-        <form method="POST" action="#" enctype="multipart/form-data">
-          @csrf
+        <form method="POST" action="#" enctype="multipart/form-data" onsubmit="return validateForm()">
+            @csrf
             <div class="input-box">
                 <input type="text" name="name" placeholder="Enter your name" required>
+                <div id="name-error" class="error-message"></div>
             </div>
             <div class="input-box">
                 <input type="text" name="email" placeholder="Enter your email" required>
+                <div id="email-error" class="error-message"></div>
             </div>
             <div class="input-box">
                 <input type="password" name="password" placeholder="Create password" required>
+                <div id="password-error" class="error-message"></div>
             </div>
             <div class="input-box">
                 <input type="password" name="password_confirmation" placeholder="Confirm password" required>
+                <div id="confirm-password-error" class="error-message"></div>
             </div>
             <div class="policy">
                 <input type="checkbox">
@@ -38,6 +50,60 @@
             </div>
         </form>
     </div>
+
+    <script>
+        function validateForm() {
+            var name = document.getElementsByName("name")[0].value;
+            var email = document.getElementsByName("email")[0].value;
+            var password = document.getElementsByName("password")[0].value;
+            var confirmPassword = document.getElementsByName("password_confirmation")[0].value;
+
+            // Clear previous error messages
+            document.getElementById('name-error').innerHTML = '';
+            document.getElementById('email-error').innerHTML = '';
+            document.getElementById('password-error').innerHTML = '';
+            document.getElementById('confirm-password-error').innerHTML = '';
+
+            if (name.length < 3) {
+                showAlert('Name must be at least 3 characters.', 'name-error');
+                return false;
+            }
+
+            if (email.length < 3 || !isValidEmail(email)) {
+                showAlert('Enter a valid email address.', 'email-error');
+                return false;
+            }
+
+            if (password.length < 8) {
+                showAlert('Password must be at least 8 characters.', 'password-error');
+                return false;
+            }
+
+            if (password !== confirmPassword) {
+                showAlert('Password confirmation does not match.', 'confirm-password-error');
+                return false;
+            }
+
+            return true;
+        }
+
+        function showAlert(message, errorDivId) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Form Validation Failed!',
+                text: message,
+                confirmButtonText: 'OK'
+            });
+            document.getElementById(errorDivId).innerHTML = message;
+        }
+
+        function isValidEmail(email) {
+            // A more permissive email validation regex
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
+    </script>    
 </body>
 
 </html>
+
